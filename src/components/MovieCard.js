9e272c1a04f4663e00762edf624cbaf37b444f8b
SoniRe <helloadityayo@gmail.com>
2023-12-31 23:13:32 +0530

    Building Core

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,9 @@ import { IMG_CDN } from "./../utils/constants";
 
 const MovieCard = ({ posterPath }) => {
   return (
-    <div className=" hover:scale-110 transition">
+    <div className="hover:scale-110 transition cursor-pointer">
       <img
-        className="w-44 mr-4 mb-1"
+        className=" w-[12vw] mb-1 rounded"
         src={IMG_CDN + posterPath}
         alt="Movie Card"
       />
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,48 @@
 import MovieCard from "./MovieCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const MovieList = ({ title, movies }) => {
+  const settings = {
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 2,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="px-6 pb-4 ">
-      <h1 className="text-3xl py-6 font-semibold text-white">{title}</h1>
-      <div className="flex overflow-x-scroll overflow-y-hidden">
-        <div className="flex flex-shrink-0">
+    <div className="px-6 pb-4">
+      <h1 className="text-3xl py-6 font-medium text-white">{title}</h1>
+      <div className="sliding">
+        <Slider {...settings}>
           {movies.map((movie) => (
             <MovieCard key={movie.id} posterPath={movie.poster_path} />
           ))}
-        </div>
+        </Slider>
       </div>
     </div>
   );
